3beb7d90b4481d8d29bed508f66e109d6a2aeb01
Farseena-M <farseenamoyan19@gmail.com>
2024-04-08 22:20:59 +0530

    updt13

--- src/components/chat/menu/conversations.js
+++ src/components/chat/menu/conversations.js
@@ -1,10 +1,10 @@
 import React, { useContext, useEffect } from 'react'
-// import { getUsers } from '../../../service/api'
 import Conversation from './conversation'
 import { Box, Divider, styled } from '@mui/material'
 import { userContext } from '../../../App'
 import { Axios } from 'axios'
 import { toast } from 'react-toastify'
+
 
 const Component = styled(Box)`
 height:81vh;
@@ -18,28 +18,20 @@ opacity:0.6;
 
 
 const Conversations = () => {
-  /*   const [users,setUsers] = useState([])
-    useEffect(()=>{
-   const fetchData = async () =>{
-    const response = await getUsers()
-    setUsers(response)
-   }
-   fetchData()
-    },[]) */
   const { user, setUser } = useContext(userContext)
-  
+
   useEffect(() => {
     const admnFetchUsers = async () => {
       try {
         const rspns = await Axios.get('http://localhost:4000/users/all')
-        console.log(rspns.data.data.findUser);
-        setUser(rspns.data.data.findUser)
+        console.log(rspns.data.data);
+        setUser(rspns.data.data)
       } catch (err) {
         toast.error(err)
       }
     }
     admnFetchUsers()
-  }, [setUser])
+  }, [])
 
   return (
     <Component>
